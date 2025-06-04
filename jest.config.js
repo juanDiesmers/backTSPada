f632be5588eb9ed99bb75eff4bb88d9f387af82f
Juan  Palacios Toledo <72896584+juanDiesmers@users.noreply.github.com>
2025-06-03 20:26:11 -0500

    Increase Jest timeout

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,7 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testMatch: ['**/tests/**/*.test.ts']
+  testMatch: ['**/tests/**/*.test.ts'],
+  // Increase timeout for slower environments
+  testTimeout: 10000
 };
